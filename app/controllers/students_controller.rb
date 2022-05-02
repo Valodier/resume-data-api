81f803f2336c8fcdc8858d47d75b8639867cdc9d
William Meyer <valodier@icloud.com>
2022-05-02 15:31:36 -0400

    Adds students controller and empty students actions

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,19 @@
+class StudentsController < ApplicationController
+
+  def index
+    
+  end
+  def create
+    
+  end
+  def show
+    
+  end
+  def update
+    
+  end
+  def destroy
+    
+  end
+
+end
