1327a5de2589fb4439b8b863f5f9d175a4f50403
William Meyer <valodier@icloud.com>
2022-05-02 15:20:39 -0400

    Adds students routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  "hello"
-  "And hello to you!"
-  "Hi"
+  
+  ##STUDENTS###
+  get  "/students" => "students#index"
+  post "/students" => "students#create"
+  get  "/students/:id" => "students#show"
+  patch "/students/:id" => "students#update"
+  delete "/students/:id" => "students#destroy"
+  
 end
