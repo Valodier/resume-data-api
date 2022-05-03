86a5d500ba4150fe5846c8bba39cefa1cc349aa3
William Meyer <valodier@icloud.com>
2022-05-03 13:36:34 -0400

    Adds students create action logic

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -4,9 +4,29 @@ class StudentsController < ApplicationController
     students = Student.all.order(:id)
     render json: students
   end
+  
   def create
-    
+    students = Student.create!(
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      email: params[:email],
+      phone_number: params[:phone_number],
+      short_bio: params[:short_bio],
+      linkedin_url: params[:linkedin_url],
+      twitter_handle: params[:twitter_handle],
+      website_url: params[:website_url],
+      online_resume_url: params[:online_resume_url],
+      github_url: params[:github_url],
+      phone_number: params[:phone_number]
+    )
+
+    if student.save
+      render json: student
+    else
+      render json: {errors: student.errors.full_messages}, status: 422
+    end
   end
+
   def show
     
   end
