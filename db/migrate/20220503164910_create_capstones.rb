c18bbc9146709d844d110996c53d2f7a19eae874
William Meyer <valodier@icloud.com>
2022-05-03 12:50:06 -0400

    redo model generations

--- db/migrate/20220503164910_create_capstones.rb
+++ db/migrate/20220503164910_create_capstones.rb
@@ -1,6 +1,10 @@
 class CreateCapstones < ActiveRecord::Migration[7.0]
   def change
     create_table :capstones do |t|
+      t.string :name
+      t.string :description
+      t.string :url
+      t.string :screenshot
 
       t.timestamps
     end
