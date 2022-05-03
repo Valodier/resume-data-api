aee34c8e9024f52fb0fa03b672495676039f1cbc
Alex <alexgmiller00@gmail.com>
2022-05-03 11:03:33 -0700

    Adds requests and updates routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,33 @@ Rails.application.routes.draw do
   get  "/students/:id" => "students#show"
   patch "/students/:id" => "students#update"
   delete "/students/:id" => "students#destroy"
+
+  ##CAPSTONES###
+  get  "/capstones" => "capstones#index"
+  post "/capstones" => "capstones#create"
+  get  "/capstones/:id" => "capstones#show"
+  patch "/capstones/:id" => "capstones#update"
+  delete "/capstones/:id" => "capstones#destroy"
+
+  ##EDUCATION###
+  get  "/education" => "education#index"
+  post "/education" => "education#create"
+  get  "/education/:id" => "education#show"
+  patch "/education/:id" => "education#update"
+  delete "/education/:id" => "education#destroy"
+
+  ##EXPERIENCE###
+  get  "/experience" => "experiences#index"
+  post "/experience" => "experiences#create"
+  get  "/experience/:id" => "experiences#show"
+  patch "/experience/:id" => "experiences#update"
+  delete "/experience/:id" => "experiences#destroy"
+
+  ##SKILLS###
+  get  "/skills" => "skills#index"
+  post "/skills" => "skills#create"
+  get  "/skills/:id" => "skills#show"
+  patch "/skills/:id" => "skills#update"
+  delete "/skills/:id" => "skills#destroy"
   
 end
