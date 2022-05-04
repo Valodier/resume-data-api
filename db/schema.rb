c22df64080ee7df316e3bdfaa8d4e37f4932159c
hector <hectornuno1994@gmail.com>
2022-05-04 12:02:29 -0700

    removes user id from skills table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_04_185434) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_04_190017) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -51,7 +51,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_04_185434) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "student_id"
-    t.integer "user_id"
   end
 
   create_table "students", force: :cascade do |t|
